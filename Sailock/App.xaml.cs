235f30b3cadab143115aaf970a5d36095085a474
Sailok25 <albaayvi4@gmail.com>
2026-07-09 22:32:16 +0200

    Corregida la ventana de Login para que tenga siempre el tema oscuro

--- Sailock/App.xaml.cs
+++ Sailock/App.xaml.cs
@@ -1,14 +1,16 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Sailock.Services;
 
 namespace Sailock
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            ThemeService.ApplyTheme(true);
 
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
 }
